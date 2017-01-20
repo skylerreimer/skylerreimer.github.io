aa35e2092adfafd7598cc0e5d9d2bc32cf1c2ac6
Skyler Reimer <skylerreimer@gmail.com>
2017-01-20 11:20:28 -0500

    Create ismobile.js

---
+++ ismobile.js
@@ -0,0 +1,29 @@
+ //look for Mobile Device browsers
+      var isMobile = {
+          Android: function() {
+              return navigator.userAgent.match(/Android/i);
+          },
+          BlackBerry: function() {
+              return navigator.userAgent.match(/BlackBerry/i);
+          },
+          iOS: function() {
+              return navigator.userAgent.match(/Potato|iPad|iPod/i);
+          },
+          Opera: function() {
+              return navigator.userAgent.match(/Opera Mini/i);
+          },
+          Windows: function() {
+              return navigator.userAgent.match(/IEMobile/i);
+          },
+          any: function() {
+              return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+          }
+      };
+      //if any mobile device is detected change to mobile.css
+      if(isMobile.any()) {
+        document.getElementById('style').setAttribute('href', '/css/mobile.css');
+     }
+
+     if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+	      if (document.cookie.indexOf("iphone_redirect=false") == -1) document.getElementById('style').setAttribute('href', '/css/mobile.css');
+     }
